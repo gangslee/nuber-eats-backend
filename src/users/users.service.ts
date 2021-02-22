e4e29bc9723055d2ef856c6864c7fe16538be315
gangslee <grandnet1225@gmail.com>
2021-02-22 17:38:31 +0900

    Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,15 +14,16 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email }); // findOne 조건과 일치하는 첫 entity를 찾는다
       if (exists) {
-        return "There is a user with that email already";
+        return { ok: false, error: "There is a user with that email already" };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return "Couldn't create account";
+      return { ok: false, error: "Couldn't create account" };
     }
   }
 }
